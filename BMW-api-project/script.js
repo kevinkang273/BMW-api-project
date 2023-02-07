c0f4cc257231b36f760a5884f0bb8ccf75f78b16
kevinkang273 <kevinkang273@gmail.com>
2023-02-07 16:42:22 -0500

    Add files via upload

--- BMW-api-project/script.js
+++ BMW-api-project/script.js
@@ -25,10 +25,27 @@ app.getBookCategoryList = () => {
 
 app.reviewBaseUrl = `https://api.nytimes.com/svc/books/v3/reviews.json`;
 
+app.overviewBaseUrl = `https://api.nytimes.com/svc/books/v3/lists/overview.json
+`;
+
+// Display image of the cover (Get top 5 books for all the Best Sellers lists for specified date.)
+app.getBookImage = () => {
+    const imageList = $.ajax({
+        url: `${app.overviewBaseUrl}`,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            apiKey: app.overviewBaseUrl
+        }
+    })
+    return imageList;
+}
+
+
 // Display the author, and a very brief description of the book.
 app.getBookReviews = () => {
     const reviews = $.ajax({
-        url: `${app.reviewBaseUrl}?isbn`,
+        url: `${app.reviewBaseUrl}?author`,
         method: 'GET',
         dataType: 'json',
         data: {
